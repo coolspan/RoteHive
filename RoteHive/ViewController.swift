9b565e15f7b3759e19a6a2db6631647472115c69
coolspan <coolspan@sina.cn>
2018-07-20 16:13:06 +0800

    NF:1.create IOS project.

---
+++ RoteHive/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RoteHive
+//
+//  Created by 乔晓松 on 2018/7/20.
+//  Copyright © 2018年 coolspan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
